4c924d0ba9e3d36b190da0d6c392a0a9fb864c37
Maria Szubski <hello@mariaszubski.com>
2019-06-21 16:26:44 -0400

    Update notes

--- src/components/layout.js
+++ src/components/layout.js
@@ -45,7 +45,7 @@ const Layout = ({ children }) => {
         {children}
       </main>
 
-      <footer style={{ textAlign: 'center' }}>
+      <footer style={{ textAlign: 'center', fontSize: '0.8rem' }}>
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a> for <a href="https://www.meetup.com/JAMstack-Cincinnati/" target="_blank" rel="noopener noreferrer">JAMstack Cincinnati</a>
