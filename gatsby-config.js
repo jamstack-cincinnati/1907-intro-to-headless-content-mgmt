263f39d55a7a79d6e4382f8524df085b38cfdb7c
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-07-21 22:39:57 -0400

    Implement Contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `JAMstack Cincinnati`,
     description: `Kick off a static site in Gatsby.`,
-    author: `JAMstack Cincinnati`,
+    author: `JAMstack Cincinnati`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,8 +13,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -25,29 +25,37 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     `gatsby-transformer-yaml`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/resources/`,
-      },
+        path: `${__dirname}/src/resources/`
+      }
     },
     // Add Web Font Loader options here
     {
-      resolve: 'gatsby-plugin-web-font-loader',
+      resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ['Dosis', 'Nunito:400,600,700']
+          families: ["Dosis", "Nunito:400,600,700"]
         }
       }
-    }
+    },
 
+    // ...
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `d1f8a2qb0dss`,
+        accessToken: `TVDIF_WpPptNG9_tgCa5kuaxEyEUlEOLO9wZANW995M`
+      }
+    }
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
