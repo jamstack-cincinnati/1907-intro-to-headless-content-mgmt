22c1a910daec98a3eafb430c037791d2065f359c
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-07-21 22:40:24 -0400

    Populate quotes from Contentful

--- src/pages/quotes.js
+++ src/pages/quotes.js
@@ -1,27 +1,41 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import QuoteBlock from "../components/quote-block"
-import data from "../resources/data.yml"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import QuoteBlock from "../components/quote-block";
 
-const QuotePage = () => (
+const QuotePage = ({ data }) => (
   <Layout>
     <SEO title="Quotes" />
     <div className="breadcrumbs">
       <Link to="/">home</Link> / quotes
     </div>
-
-    {data.map((q, idx) => (
+    {data.allContentfulQuote.edges.map((q, idx) => (
       <QuoteBlock
-        quote={q.quote}
-        name={q.name}
-        votes={q.votes}
-        key={`quote-${idx}`} />
+        quote={q.node.body.body}
+        name={q.node.attribution}
+        votes={q.node.votes}
+        key={`quote-${idx}`}
+      />
     ))}
-
   </Layout>
-)
+);
 
-export default QuotePage
+export default QuotePage;
+
+export const query = graphql`
+  query QuoteQuery {
+    allContentfulQuote {
+      edges {
+        node {
+          attribution
+          votes
+          body {
+            body
+          }
+        }
+      }
+    }
+  }
+`;
